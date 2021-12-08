186fa43e2603ead62aabeb45feb29ecf8cabb403
farhanazizf <farhanazizf7@gmail.com>
2021-12-08 19:19:11 +0700

    fix: remove unused code

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import BottomNav from "./bottom-nav";
-// import { Detector } from "react-detect-offline";
 import Styled from "./style";
-// import useToast from "./toast";
 
 export const MainLayout: React.FC<{ backButton?: boolean }> = ({
   backButton,
@@ -11,14 +9,8 @@ export const MainLayout: React.FC<{ backButton?: boolean }> = ({
 }) => {
   const history = useHistory();
 
-  // const [ToastOffline, setToast] = useToast({ offlineMode: true });
-
   return (
     <Styled.MainContainer>
-      {/* <Detector
-        onChange={() => setToast({ message: "You are offline!" })}
-        render={({ online }) => (online ? null : <ToastOffline />)}
-      /> */}
       <Styled.MainWrapper>
         {backButton ? (
           <Styled.IconChevron onClick={() => history.goBack()} />
--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -1,32 +1,14 @@
 import { Alert, Snackbar } from "@mui/material";
 import React from "react";
-import styled from "@emotion/styled";
 
 type UseToast = [React.FC, (property: { message: string }) => void];
-interface Params {
-  offlineMode?: boolean;
-}
 
 const defaultToastProperty = {
   message: "",
 };
 
-const StyledAlert = styled(Alert)`
-  &&& {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    font-size: 24px;
-    font-weight: 700;
-    height: 80px;
-    border: 1px solid rgb(95, 33, 32);
-  }
-`;
-
-function useToast(params?: Params): UseToast {
-  const [visible, setVisible] = React.useState<boolean>(
-    params?.offlineMode || false
-  );
+function useToast(): UseToast {
+  const [visible, setVisible] = React.useState<boolean>(false);
   const [toastProperty, setProperty] = React.useState<{ message: string }>(
     () => ({
       ...defaultToastProperty,
@@ -44,16 +26,12 @@ function useToast(params?: Params): UseToast {
   const Toast: React.FC = () => (
     <Snackbar
       open={visible}
-      autoHideDuration={params?.offlineMode ? null : 1500}
+      autoHideDuration={1500}
       onClose={() => setVisible(false)}
     >
-      {params?.offlineMode ? (
-        <StyledAlert severity="warning">You are offline!</StyledAlert>
-      ) : (
-        <Alert severity="error" sx={{ width: "100%", fontWeight: 700 }}>
-          {toastProperty.message}
-        </Alert>
-      )}
+      <Alert severity="error" sx={{ width: "100%", fontWeight: 700 }}>
+        {toastProperty.message}
+      </Alert>
     </Snackbar>
   );
 
