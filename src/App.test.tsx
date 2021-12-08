ed1f53cb3946ed8eb90c87bbdbeba6c091147db4
farhanazizf <farhanazizf7@gmail.com>
2021-12-08 13:39:24 +0700

    wip(unittest): create little unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, act } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import Homepage from "../src/pages/Homepage";
+import Details from "../src/pages/Details";
+import MyPokemon from "../src/pages/MyPokemon";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("render homepage without crashing", async () => {
+  const history = createMemoryHistory();
+  const { baseElement } = render(
+    <Router history={history}>
+      <Homepage />
+    </Router>
+  );
+
+  await act(async () => {
+    expect(baseElement).toBeDefined();
+  });
+});
+
+test("render details page without crashing", async () => {
+  const history = createMemoryHistory();
+  const { baseElement } = render(
+    <Router history={history}>
+      <Details />
+    </Router>
+  );
+
+  await act(async () => {
+    expect(baseElement).toBeDefined();
+  });
+});
+
+test("render my pokemon page without crashing", async () => {
+  const history = createMemoryHistory();
+  const { baseElement } = render(
+    <Router history={history}>
+      <MyPokemon />
+    </Router>
+  );
+
+  await act(async () => {
+    expect(baseElement).toBeDefined();
+  });
 });
